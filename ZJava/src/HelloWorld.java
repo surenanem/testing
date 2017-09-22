dc7fcbdb9e3b144f4230960b02113c4922b6bd3c
sanem <sanem@D160470.stjude.sjcrh.local>
2017-09-22 15:36:23 -0500

    some code added to master

--- ZJava/src/HelloWorld.java
+++ ZJava/src/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hello world 1");
+		System.out.println("hello world 1. master code updated. might need a merge with branch.");
 
 	}
 
